c92a1adbc7b418c4c7fe7ae3b9c49b5cdc068e65
zinasemia <89393314+zinasemia@users.noreply.github.com>
2022-02-03 14:46:08 +0100

    Update my.js

--- js/my.js
+++ js/my.js
@@ -1,6 +1,6 @@
 // set date we are counting
 var countDownDate = new Date("2022-06-10T00:00:00").getTime();
-const url= new URL('https://sein.fullmind.eu');
+
 
 // Update the count down every 1 second
 var x = setInterval(function () {
